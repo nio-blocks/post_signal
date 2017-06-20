935c966d300c04f35f15e070e3160d205ab48a12
Chris Brackert <cbrackert@gmail.com>
2017-06-20 15:37:06 -0600

    Add property to configure response headers for CORS

--- post_signal_block.py
+++ post_signal_block.py
@@ -2,18 +2,20 @@ from nio.block.base import Block
 from nio.signal.base import Signal
 from nio.command import command
 from nio.command.params.dict import DictParameter
-from nio.properties.string import StringProperty
-from nio.properties.int import IntProperty
+from nio.properties import *
 from nio.modules.web import RESTHandler, WebEngine
 
 
 class BuildSignal(RESTHandler):
-    def __init__(self, endpoint, notify_signals, logger):
+    def __init__(self, endpoint, notify_signals, logger, response_headers):
         super().__init__('/'+endpoint)
         self.notify_signals = notify_signals
         self.logger = logger
+        self.response_headers = response_headers
 
     def on_post(self, req, rsp):
+        rsp.set_header('Access-Control-Allow-Origin',
+                       self.response_headers().access_control_allow_origin())
         body = req.get_body()
         if isinstance(body, dict):
             body = [body]
@@ -28,7 +30,18 @@ class BuildSignal(RESTHandler):
 
     def on_options(self, req, rsp):
         """Handle OPTIONS for CORS requests"""
-        pass
+        rsp.set_header('Access-Control-Allow-Origin',
+                       self.response_headers().access_control_allow_origin())
+        rsp.set_header('Access-Control-Allow-Headers',
+                       self.response_headers().access_control_allow_headers())
+
+
+class ResponseHeaders(PropertyHolder):
+    access_control_allow_origin = StringProperty(
+        title='Access-Control-Allow-Origin', default='*')
+    access_control_allow_headers = StringProperty(
+        title='Access-Control-Allow-Headers',
+        default='Accept, Origin, Content-Type, Authorization')
 
 
 @command("post", DictParameter("signal"))
@@ -37,6 +50,8 @@ class PostSignal(Block):
     host = StringProperty(title='Host', default='0.0.0.0')
     port = IntProperty(title='Port', default=8182)
     endpoint = StringProperty(title='Endpoint', default='')
+    response_headers = ObjectProperty(
+        ResponseHeaders, title='Response Headers', default=ResponseHeaders())
 
     def __init__(self):
         super().__init__()
@@ -49,7 +64,8 @@ class PostSignal(Block):
         self._server.add_handler(
             BuildSignal(self.endpoint(),
                         self.notify_signals,
-                        self.logger),
+                        self.logger,
+                        self.response_headers),
         )
 
     def start(self):
