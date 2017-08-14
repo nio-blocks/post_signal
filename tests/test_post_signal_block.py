71ef57105ccfb94b39464793b38a18b5610a3f74
Dylan Orzel <dylan.orzel@gmail.com>
2017-08-14 10:15:45 -0600

    pep8 fixes

--- tests/test_post_signal_block.py
+++ tests/test_post_signal_block.py
@@ -80,7 +80,7 @@ class TestPostSignal(NIOBlockTestCase):
         self.assertEqual(response.set_header.call_args_list[0][0][0],
                          'Access-Control-Allow-Origin')
         self.assertEqual(response.set_header.call_args_list[0][0][1],
-                         response_headers.return_value.\
+                         response_headers.return_value.
                          access_control_allow_origin.return_value)
         self.assertEqual(len(self.last_notified[DEFAULT_TERMINAL]), 0)
 
@@ -95,11 +95,11 @@ class TestPostSignal(NIOBlockTestCase):
         self.assertEqual(response.set_header.call_args_list[0][0][0],
                          'Access-Control-Allow-Origin')
         self.assertEqual(response.set_header.call_args_list[0][0][1],
-                         response_headers.return_value.\
+                         response_headers.return_value.
                          access_control_allow_origin.return_value)
         self.assertEqual(response.set_header.call_args_list[1][0][0],
                          'Access-Control-Allow-Headers')
         self.assertEqual(response.set_header.call_args_list[1][0][1],
-                         response_headers.return_value.\
+                         response_headers.return_value.
                          access_control_allow_headers.return_value)
         self.assertEqual(len(self.last_notified[DEFAULT_TERMINAL]), 0)
